b7f553713425cbc834399f9d99fd2f157d4e597d
Your Name <you@example.com>
2024-10-20 11:26:22 +0530

    Add after get api of services

--- Controller/Service1.js
+++ Controller/Service1.js
@@ -22,4 +22,26 @@ exports.service1Create = async function (req, res, next) {
 
 }
 
+exports.Allservice = async function (req, res, next) {
 
+    try {
+
+        let Allservice = await VIDHVASAHAY.find()
+        // console.log(Allservice.map((el) => el));  
+        // console.log(Allservice.map((el) => el.villageName.name));
+
+        res.status(201).json({
+            status: "Success",
+            message: "Services Found Successfully!",
+            data: Allservice
+
+        })
+
+    } catch (error) {
+        res.status(404).json({
+            status: "Fail",
+            message: error.message
+        })
+    }
+
+}
--- routes/services1.js
+++ routes/services1.js
@@ -5,6 +5,7 @@ let services1Controller = require("../Controller/Service1")
 
 
 router.post('/create', services1Controller.service1Create);
+router.get('/all', services1Controller.Allservice);
 
 
 module.exports = router;
